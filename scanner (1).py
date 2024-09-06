48b4663eac4baeb0c0d0c915cf3cfdd53177ed31
Pietro Caruso <pietrocaruso22@gmail.com>
2024-09-06 15:26:53 +0200

    Add files via upload

---
+++ scanner (1).py
@@ -0,0 +1,121 @@
+ascii_art = ("""                                                                                                                                                                                                                                                                                                     
+
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⣀⣀⣀⣤⣤⣤⣀⣀⣀⣀⣀⣀⣀⣤⡤⠶⠶⠶⢶⣤⣤⣀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣤⣴⠶⠾⠛⠛⠋⠉⠉⠉⠉⠉⢉⡉⠉⢙⣿⡿⠛⠉⠀⣀⡀⠀⠀⠀⠀⠉⠛⠷⣦⡀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣴⠶⠛⠋⠉⠀⠀⢀⣠⣤⣄⣀⣀⣀⣤⣄⠈⠛⢀⡾⠋⠀⠀⠀⣾⡿⠃⠀⠀⠀⠀⠀⠀⠀⠘⢿⡄⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣴⠟⠋⠀⠀⠀⠀⠀⠀⢴⠟⣩⡿⢛⣉⠛⣿⣿⣿⣷⣶⠟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⣿⡄
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⡾⠋⠁⠀⠀⠀⠀⠀⠀⢀⣠⣤⣄⣿⡇⠺⣿⡇⣸⡟⠋⣿⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣷
+⠀⠀⠀⠀⠀⠀⠀⢀⣴⠏⠀⠀⠀⠀⠀⠀⣠⣾⣿⢿⣟⠛⠻⣿⣛⣳⣶⣾⣿⣦⣾⣃⠄⠀⠀⣀⣠⣤⣴⣶⡶⠶⠶⠞⠛⠛⠻⣿⣶⣄⠀⢸⣿
+⠀⠀⠀⠀⠀⠀⣠⡞⠁⠀⠀⠀⠀⠀⠀⠐⠛⠋⠀⠀⢿⣷⣦⣤⣭⣭⣷⣦⣴⣟⣉⣥⣶⠾⠟⠛⠋⠉⠁⠀⠀⠀⠀⢀⣴⣶⣿⡿⠟⠻⣷⣾⡟
+⠀⠀⠀⠀⠀⣴⠏⠀⠀⠀⠀⠀⣠⡶⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⢉⣿⣿⠿⠟⢛⣩⣵⣶⠖⠀⢰⣞⣋⣥⣴⣶⣶⣾⣿⣿⡿⠋⠀⠀⠀⢹⡟⠀
+⠀⠀⠀⢀⣾⠃⠀⠀⠀⠀⠀⣾⣿⣥⣤⡄⠀⠠⠖⢶⣶⡶⠆⠀⠀⠉⠉⠓⠛⠉⠉⢙⣿⣿⣿⡿⠛⢩⣿⣿⣿⣿⣿⣿⡟⠁⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⡠⣿⠃⠀⠀⠀⠀⠀⠀⠘⣿⣿⣏⣀⣀⣠⣤⣦⣤⠀⠀⠀⠀⠀⠀⣀⠀⠀⠀⠀⣻⣿⠃⠀⣠⣿⣿⣿⣿⣿⣿⡟⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠰⠠⢿⣄⠀⠀⠀⠀⠀⠀⠐⠞⠛⢻⣿⠿⣿⣿⣿⠁⠀⠀⢠⣶⣧⣴⣿⣷⣴⣾⣿⡿⠃⠀⣰⣿⣿⣿⡿⢿⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠐⠀⠻⠛⠉⠀⠀⠀⠰⡆⢀⣶⠀⠀⠀⠀⠸⠟⠋⠁⠀⠀⢠⣾⡿⠟⠉⠉⠉⡿⠟⠋⠀⠀⣰⣿⣿⣿⡏⠀⠀⢸⡿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠄⠀⠀⠀⠀⠀⠀⠀⠀⢠⠟⠁⠀⠀⠀⠀⠀⠀⣾⣿⠟⠁⠈⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⢰⣿⣿⡿⡿⠀⠀⠀⢺⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠂⠄⠀⠀⠀⠀⠀⠀⠀⠈⠀⠀⠀⠀⠀⠀⠀⠐⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠚⠉⠙⠀⠀⠀⠀⠀⢸⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⢀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣷⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⡰⡿⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⡀⠀⠀⠀⡀⠀⠀⠀⣼⡇⠀⠀⠀⠀⠀⣸⣿⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⢀⠟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢴⣿⡇⠀⠀⢸⣷⠀⠀⣼⣿⠇⠀⠀⠀⠀⠀⢸⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠈⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⠁⠀⠀⠘⠛⠀⢰⣿⠏⠀⠀⠀⠀⠀⠀⠀⢿⡟⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⠘⣧⠀⠀⠀⠀⠀⠀⠈⠁⠀⠀⠀⠀⠀⠀⠀⠀⠘⣷⡀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⢀⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣰⣿⠀⠈⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⣷⡄⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⣸⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⣿⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢻⣆⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⣿⡧⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠹⣿⣿⣇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⢷⡄⠀⠀⠀⠀
+⠀⠀⠆⠀⠀⠀⠀⢰⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢻⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡄⠀⠀⠀⠀⠀⠙⣦⠀⠀⠀
+⠀⠀⣿⠀⠀⠀⠀⣾⡟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠁⣠⣤⣦⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣤⣿⡄⠀⠀⠀⠀⠀⢸⠃⠀⠀
+⠐⠀⢙⣧⠀⠀⢀⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣰⠃⠀⠀⢸⣿⣿⣿⣆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠄⣄⠀⠘⣿⣿⣦⣴⣄⠀⠀⠀⠃⠀⠀
+⠀⠰⢞⣿⣆⣾⠀⠁⠀⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⡿⠀⢀⣴⣿⣿⣿⣿⣿⣿⣾⡆⠀⠠⠀⠀⠁⠀⠀⢹⡄⠀⣿⣿⣿⣿⣿⣄⣤⡄⠀⠀⠀
+⠀⠈⡽⢽⣿⡇⠀⣀⣼⣿⠀⠀⠀⠀⠀⢀⣀⡀⠀⣾⣇⣴⣿⣿⣿⣿⣿⣿⣿⣿⣿⡈⠀⢸⣇⢠⡄⠀⠀⢸⣿⣤⣿⣿⣿⣿⣿⣿⣿⣿⠀⢀⠀
+⠀⠀⠀⠀⢻⣿⣾⣿⣿⣿⢀⣷⣴⣦⣴⣿⣿⣇⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⡄⠘⣿⣌⣿⣶⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠬⠀⠀
+⠀⠀⠀⠀⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⡛⢁⢻⣿⣿⣿⣿⣿⣿⣄⢹⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣬⣹⣿⡴⠄⠀
+⠀⢀⡀⡀⣼⣿⣿⡿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠛⢡⠨⡏⠬⢟⠏⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣍⣿⣿⣿⢖⡀⠀
+⠀⠰⠆⠆⢎⡇⡿⠟⣽⡿⣿⣿⣿⡿⣿⡿⠛⠛⡉⠉⠉⠁⢂⠀⠀⠀⠀⠠⠀⣽⡿⠛⣿⣿⣿⣿⣿⣿⣿⣿⡿⢿⣿⣿⣿⣷⣯⣿⣿⣿⢃⠀⠀
+⠀⠀⠈⠁⠋⢀⠁⠠⠈⠀⠃⢿⡿⠈⠀⠈⠀⠀⠀⠀⠀⠈⠀⠀⠀⠀⠈⠀⠀⠈⠀⠁⠸⣿⡿⡿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡏⣻⣿⣿⠿⠤⠃⠈
+⠀⠀⠀⠁⠀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⠀⠄⠀⠁⢀⠁⠉⠂⠉⠈⢿⡟⠙⠿⣿⣿⡟⠻⠓⠀⠙⠟⠉⠀⠌⠈
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠂⠁⠀⠀⠈⠁⠀⠀⠀⠈⠀⠀⠀⠀⠁⠀⠀⠘⠻⠧⠀⠀⠀⠀⠀⠀⠀⠀⠀                    
+▗▄▄▄▖ ▗▄▖ ▗▖    ▗▄▄▖ ▗▄▖ ▗▖  ▗▖    ▗▄▄▄▖ ▗▄▖ ▗▄▄▖  ▗▄▄▖▗▄▄▄▖▗▄▄▖  ▗▄▄▖              
+▐▌   ▐▌ ▐▌▐▌   ▐▌   ▐▌ ▐▌▐▛▚▖▐▌    ▐▌   ▐▌ ▐▌▐▌ ▐▌▐▌   ▐▌   ▐▌ ▐▌▐▌                
+▐▛▀▀▘▐▛▀▜▌▐▌   ▐▌   ▐▌ ▐▌▐▌ ▝▜▌    ▐▛▀▀▘▐▌ ▐▌▐▛▀▚▖▐▌   ▐▛▀▀▘▐▛▀▚▖ ▝▀▚▖              
+▐▌   ▐▌ ▐▌▐▙▄▄▖▝▚▄▄▖▝▚▄▞▘▐▌  ▐▌    ▐▌   ▝▚▄▞▘▐▌ ▐▌▝▚▄▄▖▐▙▄▄▖▐▌ ▐▌▗▄▄▞▘  Powered By ELESI                                                                                                                                                                                                                                                                                                    
+┏┓  ┏┓  ┓             •     ┏┳┓       
+┣┫  ┃ ┓┏┣┓┏┓┏┓┏┏┓┏┓┏┏┓┓╋┓┏   ┃ ┏┓┏┓┏┳┓                                  
+┛┗  ┗┛┗┫┗┛┗ ┛ ┛┗ ┗┗┻┛ ┗┗┗┫   ┻ ┗ ┗┻┛┗┗                        
+""")
+
+print(ascii_art)
+
+import socket as so
+import ipaddress
+
+def validate_ip_address(address):
+    try:
+        ip = ipaddress.ip_address(address)
+        return True
+    except ValueError:
+        return False
+
+def validate_port_range(portrange):
+    try:
+        lowport, highport = map(int, portrange.split('-'))
+        if 1 <= lowport <= 65535 and 1 <= highport <= 65535 and lowport <= highport:
+            return True
+        else:
+            return False
+    except ValueError:
+        return False
+
+def scansione_range_porte():
+    target = input("\nDigitare l'indirizzo IP della macchina da analizzare: ")
+    if not validate_ip_address(target):
+        print("***L'indirizzo IP non è valido.***")
+        return
+
+    portrange = input("\nInserire un range di porte da analizzare (es 1-65535): ")
+    if not validate_port_range(portrange):
+        print("***Il range di porte non è valido.***")
+        return
+
+    lowport, highport = map(int, portrange.split('-'))
+    print("\nAnalizzo host:", target, "dalla porta", lowport, "alla porta", highport)
+    chiuse = []
+    for port in range(lowport, highport + 1):
+        s = so.socket(so.AF_INET, so.SOCK_STREAM)
+        status = s.connect_ex((target, port))
+        if status == 0:
+            print("*** Porta", port, "- Aperta ***")
+
+def scansione_porte_totali():
+    target = input("\nDigitare l'indirizzo IP della macchina da analizzare: ")
+    if not validate_ip_address(target):
+        print("\n ***L'indirizzo IP non è valido.***")
+        return
+
+    start_port = 1
+    end_port = 65535
+    print("\nAnalizzo host:", target, "dalla porta", start_port, "alla porta", end_port)
+    chiuse = []
+    for port in range(start_port, end_port + 1):
+        s = so.socket(so.AF_INET, so.SOCK_STREAM)
+        status = s.connect_ex((target, port))
+        if status == 0:
+            print("*** Porta", port, "- Aperta ***")
+
+def analisi_host():
+    while True:
+        try:
+            scelta = int(input("\n0. Esci dal programma.\n1. Analizza tutte le porte dell'indirizzo IP di una macchina.\n2. Analizza un range di porte dell'indirizzo IP di una macchina. (1-65535)\n\n|\nv\n\n"))
+            if scelta == 0:
+                return 0
+            elif scelta == 1:
+                scansione_porte_totali()
+            elif scelta == 2:
+                scansione_range_porte()
+            else:
+                print("\n***Scelta non valida. Inserisci un numero compreso tra 1 e 2 o 0 per chiudere il programma.***\n")
+        except ValueError:
+            print("\n***Input non valido. Inserisci un numero intero.***\n")
+
+if __name__ == "__main__":
+    print("Porte comuni:\nPorta 80: Web (HTTP)\nPorta 443: Web Sicuro (HTTPS)\nPorta 43: DNS (Domain Name System)\nPorta 3389: RDP (Remote Desktop Protocol)\nPorta 21: FTP (File Transfer Protocol)\nPorta 22: SSH (Secure Shell)")
+    analisi_host()
